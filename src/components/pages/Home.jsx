de18f062d185add1a296752539c6fb814ed56af9
Yazid <yazidramadan04@gmail.com>
2023-11-16 01:02:31 +0700

    everything up-to-date!

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Navbar from "../shared/Navbar";
 import { useGlobalContext } from "../context";
 import Hero from "../shared/Hero";
-/*import southernRingNebula from '../static/southernRingNebula.png';
-import cosmicCliffs from '../static/cosmicCliffs.png';*/
-import stephansQuintet from '../static/stephansQuintet.png';
+/*import southernRingNebula from '../static/southernRingNebula.png';*/
+import cosmicCliffs from '../static/cosmicCliffs.png';
+/*import stephansQuintet from '../static/stephansQuintet.png';*/
 
 const Home = () => {
   const { handleMouseChange } = useGlobalContext();
@@ -13,7 +13,7 @@ const Home = () => {
     <>
       <div
         onMouseOver={handleMouseChange}
-        className="closeSubMenu overflow-hidden w-full h-screen relative bg-center bg-cover" style={{backgroundImage: `url(${stephansQuintet})`}}>
+        className="closeSubMenu overflow-hidden w-full h-screen relative bg-center bg-cover" style={{backgroundImage: `url(${cosmicCliffs})`}}>
         <Navbar />
        <Hero />
       </div>
