e44d7c5b349811596ae2f27d098d734c555b0ba3
Yazid <yazidramadan04@gmail.com>
2023-11-15 23:38:23 +0700

    fixed navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -3,35 +3,47 @@ import { useGlobalContext } from "../context";
 import Submenu from "./Submenu";
 import Sidebar from "./Sidebar";
 import { Link } from "react-router-dom";
+import { data } from "../data";
+import { useSpring, animated } from "react-spring";
 
 const Navbar = () => {
-  const { openSubmenu, handleMouseChange } = useGlobalContext();
+  const { openSubmenu, handleMouseChange, GLOBAL_ANIMATION_DURATION } = useGlobalContext();
 
   const disyplaySubmenu = (e) => {
     e.preventDefault();
+    const width = e.target.offsetWidth;
     const title = e.target.textContent;
     const coordinates = e.target.getBoundingClientRect();
-    const center = (coordinates.left + coordinates.right - 330) / 2;
+    const center = (coordinates.left + coordinates.right - width) / 2;
     const bottom = coordinates.bottom + 15;
     openSubmenu(title, { center, bottom });
   };
+
+  const fadeIn = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: GLOBAL_ANIMATION_DURATION },
+  });
 
   return (
     <>
       <main onMouseOver={handleMouseChange} className="closeSubMenu w-full pt-6 px-20 h-20 flex justify-between items-end font-Playpen max-lg:px-10 max-lg:py-0 max-lg:items-center max-lg:h-24">
         <div className="closeSubMenu flex-[1]">
+        <animated.div style={fadeIn}>
           <h1 className="closeSubMenu text-3xl text-Silver max-lg:text-2xl"><Link to={'/'}>CosmoQuest</Link> </h1>
+        </animated.div>
         </div>
         <div className="flex justify-evenly items-center flex-[2] h-full text-Silver max-lg:hidden">
-          <li onMouseOver={disyplaySubmenu} className="list-none">
-            <button className="text-base">Cosmic Explorations</button>
+        
+        {data.map(({title, id}) => {
+             return (
+              <animated.div style={fadeIn}>
+          <li key={id} onMouseOver={disyplaySubmenu} className="list-none">
+            <button className="text-base">{title}</button>
           </li>
-          <li onMouseOver={disyplaySubmenu} className="list-none">
-            <button className="text-base">Celestial Wonders</button>
-          </li>
-          <li onMouseOver={disyplaySubmenu} className="list-none">
-            <button className="text-base">Space Heritage and Innovation</button>
-          </li>
+        </animated.div>
+             )
+        })}
         </div>
         <Sidebar />
         <Submenu />
