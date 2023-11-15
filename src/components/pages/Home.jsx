0a7325b874d7eec958b9c5fed52f361ff09ce3b9
Yazid <yazidramadan04@gmail.com>
2023-11-15 23:35:14 +0700

    home page update

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "../shared/Navbar";
 import { useGlobalContext } from "../context";
+import Hero from "../shared/Hero";
+/*import southernRingNebula from '../static/southernRingNebula.png';
+import cosmicCliffs from '../static/cosmicCliffs.png';*/
+import stephansQuintet from '../static/stephansQuintet.png';
 
 const Home = () => {
   const { handleMouseChange } = useGlobalContext();
@@ -9,10 +13,9 @@ const Home = () => {
     <>
       <div
         onMouseOver={handleMouseChange}
-        className="closeSubMenu overflow-hidden w-full h-screen bg-DarkSlateGray relative"
-      >
+        className="closeSubMenu overflow-hidden w-full h-screen relative bg-center bg-cover" style={{backgroundImage: `url(${stephansQuintet})`}}>
         <Navbar />
-        <div className="absolute top-[-48%] left-[0%] -translate-x-[-35%] translate-y-[25%] w-[55rem] h-[95rem] bg-Silver rotate-[78deg] shadow-[00000059 0px 5px 15px]"></div>
+       <Hero />
       </div>
     </>
   );
