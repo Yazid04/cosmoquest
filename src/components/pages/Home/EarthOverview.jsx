15115f447dfcc659eae78f17616a09369067c10c
Yazid <yazidramadan04@gmail.com>
2023-11-18 13:57:59 +0700

    added 2 pictures of Full disk earth image

--- src/components/pages/Home/EarthOverview.jsx
+++ src/components/pages/Home/EarthOverview.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
 
 const EarthOverview = () => {
   return (
-    <div>EarthOverview</div>
-  )
-}
+    <>
+      <section className="w-[88%] mx-auto py-20 bg-[#e6e4e4]">
+        <div className="w-full pb-10 text-center">
+          <h1 className="text-4xl font-Playpen">Earth-overview</h1>
+        </div>
+        <div className="pb-10 w-[50%] mx-auto h-[25rem]">img here...</div>
+        <div className="w-full text-center pt-10">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+            harum id expedita nobis veritatis repudiandae nesciunt ex quas magni
+            porro explicabo exercitationem voluptatum nulla dolores, nostrum
+            voluptatibus perspiciatis quisquam amet inventore laudantium iure
+            cumque neque. Soluta iusto, ipsum eos, voluptatibus impedit rem
+            nostrum repellat accusamus nihil molestiae deleniti vel amet
+            voluptatum maiores aperiam quia. Reiciendis officiis aperiam libero
+            non sequi ipsam distinctio. Quasi voluptatum consequuntur amet iure
+            temporibus hic error, ullam quibusdam voluptates asperiores delectus
+            autem dignissimos omnis sed eaque reiciendis ipsam veritatis
+            necessitatibus nesciunt dolorum distinctio! Optio tempore repellat
+            dicta provident necessitatibus nam corrupti facere saepe accusamus,
+            quidem id quam minus obcaecati laborum nemo ducimus in voluptatem
+            minima, quibusdam at. Nulla commodi cum ipsa ratione magnam
+            accusamus autem sed fugit quod dicta minus tempore perferendis
+            aliquam earum harum laboriosam deleniti, incidunt officia ullam
+            rerum, at quasi culpa? Esse quibusdam nisi explicabo ratione
+            consequuntur error suscipit inventore. Assumenda dicta itaque
+            quaerat magni! Odit delectus pariatur deleniti, reprehenderit
+            exercitationem enim neque architecto suscipit, soluta consequatur
+            omnis quod vel repellendus eveniet quo laboriosam sed magnam
+            repudiandae est atque sunt expedita aspernatur. Esse dolores et
+            consectetur, vero laudantium animi officiis non dolor provident quod
+            deleniti hic ab eos alias atque velit quas nulla inventore facere
+            odio suscipit? Consectetur ab optio perspiciatis officiis, quos
+            molestias necessitatibus ullam sint hic et in incidunt magnam vero
+            possimus aspernatur quasi eum tempora soluta non pariatur nobis!
+            Natus quam dolore est, officiis praesentium illum. Molestiae nisi
+            amet repellendus tenetur reprehenderit quisquam perferendis aperiam
+            minima illo, quos soluta quasi, provident sint recusandae corporis!
+            Nobis, at consequuntur nulla tempora blanditiis repudiandae quaerat
+            repellat sit nisi libero beatae quidem distinctio tenetur eius.
+            Dolorum commodi deleniti rem nihil, modi optio? Enim illo
+            perspiciatis labore voluptatem quos! Deleniti laudantium veniam modi
+            molestiae architecto dolore! Provident ratione est eum, veritatis
+            ipsam nesciunt eius velit?
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default EarthOverview
+export default EarthOverview;
