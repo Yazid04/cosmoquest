0b678f2d18555ec94502e0cea5cc19ec65b0b924
Yazid <yazidramadan04@gmail.com>
2023-11-22 15:51:42 +0700

    "Mars" page complete. up-to-date. For now.

--- src/components/pages/Mars/Mars.jsx
+++ src/components/pages/Mars/Mars.jsx
@@ -3,22 +3,30 @@ import Navbar from "../../shared/Navbar";
 import { useGlobalContext } from "../../context";
 import Hero from "./Hero";
 import MarsOverview from "./MarsOverview";
-import {Footer} from '../../shared/Footer'
+import { Footer } from "../../shared/Footer";
+import marsBg from './photos/marsBg.jpeg'
 
 const Mars = () => {
   const { handleMouseChange } = useGlobalContext();
-  const styles = {
-    backgroundImage: `url('https://mars.nasa.gov/assets/red_planet_bg.jpg')`,
+  const backgroundStyles = {
+    backgroundImage: `url(${marsBg})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
   };
 
-  
-
   return (
     <>
-      <section style={styles} className="closeSubMenu w-full h-screen relative" onMouseOver={handleMouseChange}>
+      <section
+        style={{
+          ...backgroundStyles,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        className="closeSubMenu w-full h-screen relative"
+        onMouseOver={handleMouseChange}
+      >
         <Navbar />
         <Hero />
       </section>
