30e61666a095f7d6b285171a2357c70f5e44a850
Yazid <yazidramadan04@gmail.com>
2023-11-26 12:31:27 +0700

    (hero, overview, and intro to exolanets sections) all made responsive

--- src/components/pages/Exoplanets/Hero.jsx
+++ src/components/pages/Exoplanets/Hero.jsx
@@ -5,7 +5,7 @@ import { useGlobalContext } from "../../context";
 const Hero = () => {
   const {handleMouseChange} = useGlobalContext();
   return (
-    <main onMouseOver={handleMouseChange} className="closeSubMenu text-start absolute top-[45%] -translate-y-[45%] left-1/2 -translate-x-1/2 w-[80%] mx-auto h-64">
+    <main onMouseOver={handleMouseChange} className="closeSubMenu text-start absolute top-[45%] -translate-y-[45%] left-1/2 -translate-x-1/2 w-[80%] mx-auto h-64 lg:text-center lg:top-[60%] lg:max-w-4xl">
       <h2 className="text-[#fff] text-3xl leading-relaxed font-Playpen mb-10">
       “Space exploration is a force of nature unto itself that no other force
         in society can rival.”
