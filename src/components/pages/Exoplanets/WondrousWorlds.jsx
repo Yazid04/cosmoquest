42f5e0081bf78dd252d75ca759a195c0aeb815f4
Yazid <yazidramadan04@gmail.com>
2023-11-26 17:06:33 +0700

    downloaded the bg image locally, instead of using external link to the image

--- src/components/pages/Exoplanets/WondrousWorlds.jsx
+++ src/components/pages/Exoplanets/WondrousWorlds.jsx
@@ -7,15 +7,14 @@ import Exoplanets from './Exoplanets'
 import Overview from "./Overview";
 import {Footer} from '../../shared/Footer'
 import { useGlobalContext } from "../../context";
+import wondrousworldsBgImg from './imgs/wondrousworldsBgImg.jpeg' 
 
 const WondrousWorlds = () => {
-  //const img2 = "https://storage.googleapis.com/pod_public/1300/120656.jpg";
-  //const img3 = "https://img.freepik.com/premium-photo/alien-world-exoplanet-planet-landscape-digital-painting-artwork_743855-1727.jpg";
-  const img5 = "https://static01.nyt.com/images/2019/06/25/science/14exoplanets1/merlin_156410241_271b8bea-5efb-4622-a1c4-a0463590202a-superJumbo.jpg";
-  
+  // 2 extra cool bg images to use in the future (in the images folder and commented out in lines 3 and 4)
+
   const {handleMouseChange}  = useGlobalContext();
   const backgroundStyles = {
-    backgroundImage: `url(${img5})`,
+    backgroundImage: `url(${wondrousworldsBgImg})`,
     backgroundSize: "cover",
     backgroundPosition: "top",
     backgroundRepeat: "no-repeat",
