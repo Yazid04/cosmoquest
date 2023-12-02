668d7bf9c30758eb91e525d2ef63ccb31d38dff8
Yazid <yazidramadan04@gmail.com>
2023-12-02 15:15:40 +0700

    added a hero content

---
+++ src/components/pages/ISS/Hero.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { usePagesContext } from "../pagesContext";
+import { useSpring, animated } from "react-spring";
+import { useGlobalContext } from "../../context";
+
+const Hero = () => {
+  const { GLOBAL_ANIMATION_DURATION } = useGlobalContext();
+  const { SOLAR_SYSTEM_INRO_POSITION } = usePagesContext();
+
+  const scrollToTarget = () => {
+    window.scrollTo({
+      top: SOLAR_SYSTEM_INRO_POSITION,
+      behavior: "smooth",
+    });
+  };
+
+  const fadeIn = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: GLOBAL_ANIMATION_DURATION },
+  });
+
+  function yearsInOrbit() {
+    // Launch date of the ISS
+    const launchDate = new Date('1998-11-20');
+
+    // Current date
+    const currentDate = new Date();
+
+    // Calculate the number of milliseconds in a year
+    const millisecondsInYear = 1000 * 60 * 60 * 24 * 365.25;
+
+    // Calculate the number of years
+    const yearsInOrbit = (currentDate - launchDate) / millisecondsInYear;
+
+    return Math.floor(yearsInOrbit);
+}
+
+
+  return (
+    <div className="closeSubMenu w-[80%] font-Playpen h-96 text-center grid place-content-center absolute top-[50%] -translate-y-[50%] left-1/2 -translate-x-1/2 text-Silver md:top-[60%] md:-translate-y-[60%]">
+      <animated.div style={fadeIn}>
+        <h2 className="closeSubMenu text-5xl mb-5 md:text-7xl">
+          International Space Station:
+        </h2>
+        <p className="text-xl mb-12 text-[white] mx-auto bg-[black] p-3 max-w-md rounded-md">
+          {" "}
+            {yearsInOrbit() + ' Years in Orbit!'}          
+       </p>
+      </animated.div>
+      <div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/pages/ISS/InternationalSpaceStation.jsx
+++ src/components/pages/ISS/InternationalSpaceStation.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../shared/Navbar";
+import { useGlobalContext } from "../../context";
+import Hero from "./Hero";
+//import SSOverview from "./SSOverview";
+import { Footer } from "../../shared/Footer";
 
 const InternationalSpaceStation = () => {
-  return (
-    <div>InternationalSpaceStation</div>
-  )
-}
+  const { handleMouseChange } = useGlobalContext();
+  const backgroundStyles = {
+    backgroundImage: `url(${'https://www.nasa.gov/wp-content/uploads/2023/03/bwhi1apicaaamlo.jpg_large.jpg'})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundColor: "orange !important"
+  };
 
-export default InternationalSpaceStation
+  return (
+    <>
+      <section
+        style={{
+          ...backgroundStyles,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        className="closeSubMenu w-full h-screen relative"
+        onMouseOver={handleMouseChange}
+      >
+        <Navbar />
+        <Hero />
+      </section>
+      {/*<SSOverview />*/} 
+      <Footer />
+    </>
+  );
+};
+
+export default InternationalSpaceStation;
