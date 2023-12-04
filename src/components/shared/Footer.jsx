2f4dde1dd2cdf0eda219d4ec1c6b625606289ecc
Yazid <yazidramadan04@gmail.com>
2023-12-04 13:11:26 +0700

    added link to Gmail

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -4,6 +4,13 @@ import {RiCopyrightLine} from 'react-icons/ri'
 export const Footer = () => {
     const date = new Date().getFullYear();
 
+  const emailAddress = "yazidramadan04@gmail.com";
+
+  const handleEmailClick = () => {
+    window.location.href = `mailto:${emailAddress}`;
+  };
+
+
   return (
     <section className={`w-full h-min font-sourceSansPro bg-Silver max-w-[85rem] mx-auto grid place-content-center`}>
        <div className='w-min h-min px-3'>
@@ -20,9 +27,12 @@ export const Footer = () => {
            <h1 className='text-3xl font-bold'>CosmoQuest</h1>
          </div>
          </div>
-         <div className='flex items-center justify-center h-10'>
+         <div className='flex flex-col items-center justify-center h-24'>
+           <a onClick={handleEmailClick} href={`mailto:${emailAddress}`} className='py-3 cursor-pointer'>yazidramadan04@gmail.com</a> 
+          <div className='flex gap-x-2'>
            <RiCopyrightLine className='h-full' />
            <p>Copyright {date}. All rights reserved.</p>
+          </div>
         </div>
        </div>
     </section>
